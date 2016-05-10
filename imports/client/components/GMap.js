acfb44dc5e36cba19090ec06e6dbcd0cbebd4740
Josh Owens <joshua.owens@gmail.com>
2016-05-10 09:27:56 -0400

    Get map working

---
+++ imports/client/components/GMap.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
+import {default as ScriptjsLoader} from "react-google-maps/lib/async/ScriptjsLoader";
+
+export default function GMap (props) {
+  return (
+    <ScriptjsLoader
+      hostname={"maps.googleapis.com"}
+      pathname={"/maps/api/js"}
+      query={{v: '3.23', libraries: "geometry,drawing,places"}}
+      loadingElement={
+        <div>
+          <p>Loading...</p>
+        </div>
+      }
+      containerElement={
+        <div style={{height: "100%",width: "100%"}}/>
+      }
+      googleMapElement={
+        <GoogleMap
+          defaultZoom={16}
+          center={{lat: props.loc.lat, lng: props.loc.lng}}
+        >
+          {props.places.map(function(place) {
+            return <Marker position={{lat: place.geometry.location.lat,lng: place.geometry.location.lng}} key={place.name} defaultAnimation="2"/>
+          })}
+        </GoogleMap>
+      }
+    />
+  );
+}
--- imports/client/routes.js
+++ imports/client/routes.js
@@ -3,11 +3,13 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import AppContainer from '/imports/client/containers/App.js';
 import Places from '/imports/client/components/Places.js';
+import GMap from '/imports/client/components/GMap.js';
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={AppContainer}>
       <IndexRoute component={ Places } />
+      <Route path="/map" component={ GMap } />
     </Route>
   </Router>
-  );
+);
