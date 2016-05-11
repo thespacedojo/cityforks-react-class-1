abde46d87577a07f965e9e65214ca45f3346ead3
Josh Owens <joshua.owens@gmail.com>
2016-05-11 10:06:58 -0400

    Last bit from class

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+    id: 'com.cityforks.prod',
+    name: 'CityForks',
+    description: 'Find where to eat.',
+    author: 'Josh Owens',
+    email: 'joshua.owens@gmail.com',
+    website: 'https://eatwithcityforks.co',
+    version: '1.0.0'
+});
+
+
+// App.preference({'Orientation': 'portrait'});
+App.accessRule('https://eatwithcityforks.co/*');
